278a02a602e918a2fe89c8ecaf239f1a811968f3
LDHoangHuy <ledanghoanghuy2004@gmail.com>
2025-08-01 16:54:25 +0700

    feat(posts): implement get multiple posts, update post, delete post, like and unlike post functionalities

---
+++ backend/src/modules/posts/dto/filter-posts.dto.ts
@@ -0,0 +1,43 @@
+import {
+  IsOptional,
+  IsEnum,
+  IsBoolean,
+  IsNumber,
+  IsString,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+export enum SortBy {
+  NEWEST = 'newest',
+  OLDEST = 'oldest',
+}
+
+export class FilterPostsDto {
+  @IsOptional()
+  @IsString()
+  keyword?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  @Type(() => Boolean)
+  following?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  @Type(() => Boolean)
+  savedBy?: boolean;
+
+  @IsOptional()
+  @IsEnum(SortBy)
+  sortBy?: SortBy;
+
+  @IsOptional()
+  @IsNumber()
+  @Type(() => Number)
+  limit?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Type(() => Number)
+  page?: number;
+}
--- backend/src/modules/posts/posts.controller.ts
+++ backend/src/modules/posts/posts.controller.ts
@@ -1,24 +1,92 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from '@prisma/client';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import { CreatePostDto } from './dto/create-post.dto';
 import { PostsService } from './posts.service';
 import { OptionalAuth } from 'src/common/decorators/optional-auth.decorator';
+import { FilterPostsDto } from './dto/filter-posts.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Controller('posts')
 export class PostsController {
-    constructor(private readonly postsService: PostsService) {}
+  constructor(private readonly postsService: PostsService) {}
 
   @Post()
   @UseGuards(AuthGuard('jwt'))
-  async create(@CurrentUser('user') user: User, @Body('post') dto: CreatePostDto) {
-    return this.postsService.createPost(user.id, dto);
+  async create(
+    @CurrentUser('user') user: User,
+    @Body('post') createPostDto: CreatePostDto,
+  ) {
+    return this.postsService.createPost(user.id, createPostDto);
   }
 
   @Get(':postId')
   @OptionalAuth()
   async getById(@Param('postId', ParseIntPipe) postId: number) {
     return this.postsService.getPostById(postId);
+  }
+
+  @Get()
+  @OptionalAuth()
+  async getAll(
+    @Query() filter: FilterPostsDto,
+    @CurrentUser('user') user?: User,
+  ) {
+    return user
+      ? this.postsService.getPosts(user.id, filter)
+      : this.postsService.getPosts(filter);
+  }
+
+  @Put(':postId')
+  @UseGuards(AuthGuard('jwt'))
+  async update(
+    @Param('postId', ParseIntPipe) postId: number,
+    @CurrentUser('user') user: User,
+    @Body('post') updatePostDto: UpdatePostDto,
+  ) {
+    return this.postsService.updatePost(user.id, postId, updatePostDto);
+  }
+
+  @Delete(':postId')
+  @UseGuards(AuthGuard('jwt'))
+  @HttpCode(200)
+  async delete(
+    @Param('postId', ParseIntPipe) postId: number,
+    @CurrentUser('user') user: User,
+  ) {
+    return this.postsService.deletePost(user.id, postId);
+  }
+
+  @Post(':postId/like')
+  @UseGuards(AuthGuard('jwt'))
+  @HttpCode(200)
+  async like(
+    @Param('postId', ParseIntPipe) postId: number,
+    @CurrentUser('user') user: User,
+  ) {
+    return this.postsService.likePost(user.id, postId);
+  }
+
+  @Delete(':postId/unlike')
+  @UseGuards(AuthGuard('jwt'))
+  @HttpCode(200)
+  async unlike(
+    @Param('postId', ParseIntPipe) postId: number,
+    @CurrentUser('user') user: User,
+  ) {
+    return this.postsService.unlikePost(user.id, postId);
   }
 }
